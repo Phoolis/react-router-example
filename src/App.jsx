744894541b945fc76b09310fb21619efeef4b790
Phoolis <carlsonps@gmail.com>
2024-10-08 17:38:54 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import { Link, Outlet } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <nav>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/contact"}>Contact</Link>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
+
+export default App
